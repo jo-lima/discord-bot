00f1c610315e47d6a544b6625f825e30626a314c
nyxfelinto <nyx7390@gmail.com>
2025-09-24 20:24:09 -0300

    primeiras reações

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import discord
+from ascii import Ascii
+
 
 class Client(discord.Client):
-    async def on_ready(self):
-        print(f"Oi amigos lixo, eu, {self.user}, cheguei!!!")
+    def __init__(self):
+        main_channel_id = 1420531838845194312
+        main_channel = Client.get_channel(main_channel_id)
+    
     async def on_message(self, message):
-        if message.content == "penis":
-            print(f"{message.author} é um viadão!")
+        if message.author == self.user:
+            return
+        if message.content.startswith('&'):
+            mensagem = message.content[1:]
+            match mensagem:
+                case 'pintogames':
+                    await message.channel.send(Ascii.PINTO)
+    async def on_member_join(member):
+        await main_channel.send(f"olá amigo {member} você é um viadão de qualidade hein! fiu fiu!! safado to de olho em você hein")
+    async def on_reaction_add(self, reaction, user):
+        
+        if reaction == ':eggplant:':
+            main_channel.send(Ascii.PINTO)
+        
+    
+        
+        
         
 intents = discord.Intents.default()
 intents.message_content = True
 
+key = open("key.txt").read()
+
 client = Client(intents=intents)
-client.run()
+client.run(key)
